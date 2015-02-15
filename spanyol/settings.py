c51b878f6ba6b81f34f9c08207657a495f6ad195
Arie <prasetyo.mailbox@gmail.com>
2015-02-16 00:02:11 +0700

    init spanyol

---
+++ spanyol/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for spanyol project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'spanyol'
+
+SPIDER_MODULES = ['spanyol.spiders']
+NEWSPIDER_MODULE = 'spanyol.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'spanyol (+http://www.yourdomain.com)'
